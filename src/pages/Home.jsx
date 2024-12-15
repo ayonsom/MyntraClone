14a60e82807d3789ed98d9ea006265b743881460
ayonsom <140237905+ayonsom@users.noreply.github.com>
2024-12-15 12:35:25 +0530

    imports on home were fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,67 @@
 import React from 'react'
 import coupon500 from '../components/images/coupon500off.webp'
+import eoss_poster from "../components/images/eoss_poster.webp"
+import sponsor_poster_1 from "../components/images/sponsor_poster_1.webp"
+import sponsor_poster_2 from "../components/images/sponsor_poster_2.webp"
+import sponsor_poster_3 from "../components/images/sponsor_poster_3.webp"
+import discount_axis from "../components/images/discount-axis.webp"
+import discount_kotak from "../components/images/discount-kotak.webp"
+import discount_sbi from "../components/images/discount-sbi.webp"
+import discount_hdfc from "../components/images/discount-hdfc.webp"
+import discount_myntra from "../components/images/discount-myntra.webp"
+import discount_paytm from "../components/images/discount-paytm.webp"
+import discount_simpl from "../components/images/discount-simpl.webp"
+import price_crash_on_top_brands from "../components/images/price-crash-on-top-brands.webp"
+import scrollings from '../components/images/scrollings.webp'
+import first_time_on_discount from '../components/images/First-Time-On-Discount.webp'
+import first_time_on_discount_item_1 from '../components/images/First-Time-On-Discount-Item-1.webp'
+import first_time_on_discount_item_2 from '../components/images/First-Time-On-Discount-Item-2.webp'
+import first_time_on_discount_item_3 from '../components/images/First-Time-On-Discount-Item-3.webp'
+import first_time_on_discount_item_4 from '../components/images/First-Time-On-Discount-Item-4.webp'
+import first_time_on_discount_item_5 from '../components/images/First-Time-On-Discount-Item-5.webp'
+import first_time_on_discount_item_6 from '../components/images/First-Time-On-Discount-Item-6.webp'
+import first_time_on_discount_item_7 from '../components/images/First-Time-On-Discount-Item-7.webp'
 
+import shop_by_category from "../components/images/shop_by_category.webp"
+import cat_r1_c1 from "../components/images/cat_r1_c1.webp"
+import cat_r1_c2 from "../components/images/cat_r1_c2.webp"
+import cat_r1_c3 from "../components/images/cat_r1_c3.webp"
+import cat_r1_c4 from "../components/images/cat_r1_c4.webp"
+import cat_r1_c5 from "../components/images/cat_r1_c5.webp"
+
+import cat_r2_c1 from "../components/images/cat_r2_c1.webp"
+import cat_r2_c2 from "../components/images/cat_r2_c2.webp"
+import cat_r2_c3 from "../components/images/cat_r2_c3.webp"
+import cat_r2_c4 from "../components/images/cat_r2_c4.webp"
+import cat_r2_c5 from "../components/images/cat_r2_c5.webp"
+
+import cat_r3_c1 from "../components/images/cat_r3_c1.webp"
+import cat_r3_c2 from "../components/images/cat_r3_c2.webp"
+import cat_r3_c3 from "../components/images/cat_r3_c3.webp"
+import cat_r3_c4 from "../components/images/cat_r3_c4.webp"
+import cat_r3_c5 from "../components/images/cat_r3_c5.webp"
+
+import cat_r4_c1 from "../components/images/cat_r4_c1.webp"
+import cat_r4_c2 from "../components/images/cat_r4_c2.webp"
+import cat_r4_c3 from "../components/images/cat_r4_c3.webp"
+import cat_r4_c4 from "../components/images/cat_r4_c4.webp"
+import cat_r4_c5 from "../components/images/cat_r4_c5.webp"
+
+import cat_r5_c1 from "../components/images/cat_r5_c1.webp"
+import cat_r5_c2 from "../components/images/cat_r5_c2.webp"
+import cat_r5_c3 from "../components/images/cat_r5_c3.webp"
+import cat_r5_c4 from "../components/images/cat_r5_c4.webp"
+import cat_r5_c5 from "../components/images/cat_r5_c5.webp"
+
+import cat_r6_c1 from "../components/images/cat_r6_c1.webp"
+import cat_r6_c2 from "../components/images/cat_r6_c2.webp"
+import cat_r6_c3 from "../components/images/cat_r6_c3.webp"
+import cat_r6_c4 from "../components/images/cat_r6_c4.webp"
+import cat_r6_c5 from "../components/images/cat_r6_c5.webp"
+
+import cat_r7_1 from "../components/images/cat_r7_1.webp"
+import cat_r7_2 from "../components/images/cat_r7_2.webp"
+import cat_r7_3 from "../components/images/cat_r7_3.webp"
 
 export default function Home() {
   return (
@@ -9,99 +70,102 @@ export default function Home() {
         <img className='coupon-500' src={coupon500} alt="" />
       </a>
       <a href="https://www.myntra.com/myntra-fashion-store">
-        <img className='eoss-poster' src="src\components\images\eoss_poster.webp" alt="" />
+        <img className='eoss-poster' src={eoss_poster} alt="" />
       </a>
       
       <div className="sponsors">
         <a href="https://www.myntra.com/uspa?rawQuery=uspa&sort=popularity">
-          <img src="src\components\images\sponsor_poster_1.webp" alt="" />
+          <img src={sponsor_poster_1} alt="" />
         </a>
         <a href="https://www.myntra.com/myntra?f=Brand%3AGarnier%2CLOreal%2CMaybelline&sort=discount&pinProductsCsv=12371142%2C19202682%2C12371154%2C27027858%2C27027860%2C12371164%2C19202686%2C25439370%2C20588628%2C13324950%2C19469328%2C19469318%2C14172258%2C19469322%2C11178294%2C8987497%2C20292944%2C29942013%2C29225062%2C19444472%2C18987452%2C27464212%2C7638893%2C1963241%2C9747207%2C11178488%2C2230636%2C10849052%2C13609228%2C17373088%2C30322064%2C12388382%2C30508614%2C30508615%2C14172246%2C30510419%2C13324924">
-          <img src="src\components\images\sponsor_poster_2.webp" alt="" />
+          <img src={sponsor_poster_2} alt="" />
         </a>
         <a href="https://www.myntra.com/adidas?f=Categories%3ABackpacks%2CBodysuit%2CBra%2CBriefs%2CCaps%2CCasual%20Shoes%2CDresses%2CDuffel%20Bag%2CFlip%20Flops%2CHat%2CJackets%2CSandals%2CShorts%2CSkirts%2CSocks%2CSports%20Sandals%2CSports%20Shoes%2CSweatshirts%2CSwim%20Bottoms%2CSwimwear%2CTights%2CTops%2CTrack%20Pants%2CTracksuits%2CTrousers%2CTshirts%2CWaist%20Pouch&rawQuery=adidas&sort=discount">
-          <img src="src\components\images\sponsor_poster_3.webp" alt="" />
+          <img src={sponsor_poster_3} alt="" />
         </a>
         
       </div>
       <div className="bank-discounts">
-        <img src="src\components\images\discount-axis.webp" alt="" />
-        <img src="src\components\images\discount-kotak.webp" alt="" />
-        <img src="src\components\images\discount-sbi.webp" alt="" />
-        <img src="src\components\images\discount-hdfc.webp" alt="" />
-        <img src="src\components\images\discount-myntra.webp" alt="" />
-        <img src="src\components\images\discount-paytm.webp" alt="" />
-        <img src="src\components\images\discount-simpl.webp" alt="" />
+        <img src={discount_axis} alt="" />
+        <img src={discount_kotak} alt="" />
+        <img src={discount_sbi} alt="" />
+        <img src={discount_hdfc} alt="" />
+        <img src={discount_myntra} alt="" />
+        <img src={discount_paytm} alt="" />
+        <img src={discount_simpl} alt="" />
       </div>
-      <img className='price-crash-poster' src="src\components\images\price-crash-on-top-brands.webp" alt="" />
+      <img className='price-crash-poster' src={price_crash_on_top_brands} alt="" />
       <div className="scrolls">        
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>
-            <img src='src\components\images\scrollings.webp'/>          
+            <img src={scrollings}/>
+            <img src={scrollings}/>
+            <img src={scrollings}/>
+            <img src={scrollings}/>
+            <img src={scrollings}/>
+            <img src={scrollings}/>
+            <img src={scrollings}/>          
       </div>
-      <img className='First-Time-On-Discount' src="src\components\images\First-Time-On-Discount.webp" alt="" />
+      <img className='First-Time-On-Discount' src={first_time_on_discount} alt="" />
       <div className="First-Time-On-Discount-Items-Container">
-        <img src="src\components\images\First-Time-On-Discount-Item-1.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-2.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-3.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-4.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-5.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-6.webp" alt="" />
-        <img src="src\components\images\First-Time-On-Discount-Item-7.webp" alt="" />
+        <img src={first_time_on_discount_item_1} alt="" />
+        <img src={first_time_on_discount_item_2} alt="" />
+        <img src={first_time_on_discount_item_3} alt="" />
+        <img src={first_time_on_discount_item_4} alt="" />
+        <img src={first_time_on_discount_item_5} alt="" />
+        <img src={first_time_on_discount_item_6} alt="" />
+        <img src={first_time_on_discount_item_7} alt="" />
       </div>
 
-      <img className='shop-by-category' src="src\components\images\shop_by_category.webp" alt="" />
+      <img className='shop-by-category' src={shop_by_category} alt="" />
 
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r1_c1.webp" alt="" />
-        <img src="src\components\images\cat_r1_c2.webp" alt="" />
-        <img src="src\components\images\cat_r1_c3.webp" alt="" />
-        <img src="src\components\images\cat_r1_c4.webp" alt="" />
-        <img src="src\components\images\cat_r1_c5.webp" alt="" />
+        <img src={cat_r1_c1} alt="" />
+        <img src={cat_r1_c2} alt="" />
+        <img src={cat_r1_c3} alt="" />
+        <img src={cat_r1_c4} alt="" />
+        <img src={cat_r1_c5} alt="" />
       </div>
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r2_c1.webp" alt="" />
-        <img src="src\components\images\cat_r2_c2.webp" alt="" />
-        <img src="src\components\images\cat_r2_c3.webp" alt="" />
-        <img src="src\components\images\cat_r2_c4.webp" alt="" />
-        <img src="src\components\images\cat_r2_c5.webp" alt="" />
+        <img src={cat_r2_c1} alt="" />
+        <img src={cat_r2_c2} alt="" />
+        <img src={cat_r2_c3} alt="" />
+        <img src={cat_r2_c4} alt="" />
+        <img src={cat_r2_c5} alt="" />
       </div>
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r3_c1.webp" alt="" />
-        <img src="src\components\images\cat_r3_c2.webp" alt="" />
-        <img src="src\components\images\cat_r3_c3.webp" alt="" />
-        <img src="src\components\images\cat_r3_c4.webp" alt="" />
-        <img src="src\components\images\cat_r3_c5.webp" alt="" />
+        <img src={cat_r3_c1} alt="" />
+        <img src={cat_r3_c2} alt="" />
+        <img src={cat_r3_c3} alt="" />
+        <img src={cat_r3_c4} alt="" />
+        <img src={cat_r3_c5} alt="" />
+        
       </div>
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r4_c1.webp" alt="" />
-        <img src="src\components\images\cat_r4_c2.webp" alt="" />
-        <img src="src\components\images\cat_r4_c3.webp" alt="" />
-        <img src="src\components\images\cat_r4_c4.webp" alt="" />
-        <img src="src\components\images\cat_r4_c5.webp" alt="" />
+        <img src={cat_r4_c1} alt="" />
+        <img src={cat_r4_c2} alt="" />
+        <img src={cat_r4_c3} alt="" />
+        <img src={cat_r4_c4} alt="" />
+        <img src={cat_r4_c5} alt="" />
+        
       </div>
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r5_c1.webp" alt="" />
-        <img src="src\components\images\cat_r5_c2.webp" alt="" />
-        <img src="src\components\images\cat_r5_c3.webp" alt="" />
-        <img src="src\components\images\cat_r5_c4.webp" alt="" />
-        <img src="src\components\images\cat_r5_c5.webp" alt="" />
+        <img src={cat_r5_c1} alt="" />
+        <img src={cat_r5_c2} alt="" />
+        <img src={cat_r5_c3} alt="" />
+        <img src={cat_r5_c4} alt="" />
+        <img src={cat_r5_c5} alt="" />
+        
       </div>
       <div className="shop-by-cat-container-1">
-        <img src="src\components\images\cat_r6_c1.webp" alt="" />
-        <img src="src\components\images\cat_r6_c2.webp" alt="" />
-        <img src="src\components\images\cat_r6_c3.webp" alt="" />
-        <img src="src\components\images\cat_r6_c4.webp" alt="" />
-        <img src="src\components\images\cat_r6_c5.webp" alt="" />
+        <img src={cat_r6_c1} alt="" />
+        <img src={cat_r6_c2} alt="" />
+        <img src={cat_r6_c3} alt="" />
+        <img src={cat_r6_c4} alt="" />
+        <img src={cat_r6_c5} alt="" />
       </div>
       <div className="shop-by-cat-container-2">
-        <img src="src\components\images\cat_r7_1.webp" alt="" />
-        <img src="src\components\images\cat_r7_2.webp" alt="" />
-        <img src="src\components\images\cat_r7_3.webp" alt="" />
+        <img src={cat_r7_1} alt="" />
+        <img src={cat_r7_2} alt="" />
+        <img src={cat_r7_3} alt="" />
       </div>
 
 
